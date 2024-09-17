69dcdcbf0cf66457f3347c38ad687e99da78fbb7
Yetdev27 <143490338+Yetdev27@users.noreply.github.com>
2024-09-17 11:24:43 +0300

    Update Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -25,7 +25,7 @@ const Search = () => {
       const data = await fetchUserData(username, location, minRepos);
       setUserData(data.items);
     } catch (err) {
-      setError('Looks like we can\'t find the user');
+      setError('Looks like we cant find the user');
     } finally {
       setLoading(false);
     }
